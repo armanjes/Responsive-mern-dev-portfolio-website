a7aa15624480ea5aee2cefb5aa2fa662cf2a6cf1
Arman <mdarmanjes@gmail.com>
2024-09-12 01:47:25 +0600

    Home, About, Contact, Footer

---
+++ Portfolio/src/main.js
@@ -0,0 +1,14 @@
+// css
+import "../styles/normalize.css"
+import "../styles/style.css"
+import "../styles/components/home.css"
+import "../styles/components/about.css"
+import "../styles/components/skills.css"
+import "../styles/components/projects.css"
+import "../styles/components/contact.css"
+import "../styles/components/footer.css"
+import "../styles/utils.css"
+
+// JavaScript
+import "./icons.js";
+// import "./mobile.js";
