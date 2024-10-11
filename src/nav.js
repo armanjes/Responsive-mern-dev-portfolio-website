0eab1b96c67d9618d6ca4cc5960426363d1c92d1
Arman <mdarmanjes@gmail.com>
2024-10-11 19:14:30 +0600

    nav bar fixed

--- src/nav.js
+++ src/nav.js
@@ -8,10 +8,8 @@ showBtn.addEventListener("click", function () {
   if (showBtn.getAttribute("icon") === "mdi:menu") {
     showBtn.setAttribute("icon", "mdi:close");
     menuList.classList.add("show-menu");
-    menuList.classList.remove("hide-menu");
   } else {
     showBtn.setAttribute("icon", "mdi:menu");
-    menuList.classList.add("hide-menu");
     menuList.classList.remove("show-menu");
   }
 });
